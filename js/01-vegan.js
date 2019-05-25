085110434557b3f037d5b23802e6d8f2cfc9e9bd
MiriamBec <miriambec8@outlook.es>
2019-05-24 18:54:13 -0700

    Creamos comidas.html

--- js/01-vegan.js
+++ js/01-vegan.js
@@ -4,6 +4,7 @@ var theme = document.getElementById('theme');
 var theme_inf = document.getElementById('theme-inf-vegano');
 var theme_postres = document.getElementById('theme-postres');
 var theme_tartas = document.getElementById('theme-tartas');
+var theme_receta_tarta = document.getElementById('theme-receta-tarta');
 var facebookC = document.getElementById('facebookC');
 var instagramC = document.getElementById('instagramC');
 var twitterC = document.getElementById('twitterC');
@@ -59,6 +60,9 @@ function setDarkMode() {
     if (window.location.href.indexOf('tartas') > -1) {
         theme_tartas.setAttribute('href', 'css/tartas-dark.css');
     }
+    if (window.location.href.indexOf('tarta-chocolate') > -1) {
+        theme_receta_tarta.setAttribute('href', 'css/receta-tarta-dark.css');
+    }
     facebookC.setAttribute('src', 'imagenes/facebookC.png');
     instagramC.setAttribute('src', 'imagenes/instagramC.png');
     twitterC.setAttribute('src', 'imagenes/twitterC.png');
@@ -77,6 +81,9 @@ function setLightMode() {
     }
     if (window.location.href.indexOf('tartas') > -1) {
         theme_tartas.setAttribute('href', 'css/tartas.css')
+    }
+    if (window.location.href.indexOf('tarta-chocolate') > -1) {
+        theme_receta_tarta.setAttribute('href', 'css/receta-tarta.css');
     }
     facebookC.setAttribute('src', 'imagenes/facebook1.png');
     instagramC.setAttribute('src', 'imagenes/instagram1.png');
@@ -112,6 +119,24 @@ if (window.location.href.indexOf('postres') > -1) {
 // TARTAS
 
 if(window.location.href.indexOf('tartas') > -1) {
+    var las_tartas2 = document.getElementsByClassName('las-tartas-2');
+
+    for(var i = 0; i < las_tartas2.length; i++) {
+        las_tartas2[i].addEventListener('mouseover', function() {
+            this.style.background = 'rgba(95, 207, 4, 0.5)';
+            this.getElementsByClassName('img-over')[0].style.display = 'block';
+        });
+
+        las_tartas2[i].addEventListener('mouseout', function () {
+            this.style.background = 'none';
+            this.getElementsByClassName('img-over')[0].style.display = 'none';
+        });
+    }
+}
+
+//comidas
+
+if(window.location.href.indexOf('comidas') > -1) {
     var las_tartas2 = document.getElementsByClassName('las-tartas-2');
 
     for(var i = 0; i < las_tartas2.length; i++) {
